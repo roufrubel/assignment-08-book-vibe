ba35ecc0bfc15645da4bde8c3d3003728825d585
roufrubel <rubel.ma2012@gmail.com>
2024-05-22 06:52:29 +0600

    books data loaded and displayed

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,11 @@
+
+
+const Book = ({book}) => {
+    return (
+        <div>
+            <h2>{book.bookName}</h2>
+        </div>
+    );
+};
+
+export default Book;
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 
 const Books = () => {
+    const [books, setBooks] = useState([]);
+    useEffect( () => {
+        fetch('books.json')
+        .then(res => res.json())
+        .then(data => setBooks(data));
+    }, [])
     return (
-        <div>
-            <h4>Books</h4>
+        <div className="mt-20">
+            <h4 className="text-[40px] font-bold text-center">Books {books.length}</h4>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {
+                    books.map(book => <Book key={book.id} book={book}></Book>)
+                }
+            </div>
         </div>
     );
 };
