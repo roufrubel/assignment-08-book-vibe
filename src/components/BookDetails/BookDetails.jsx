e9c9290034a5862e8798dd9df2c3e05bc58b87d0
roufrubel <rubel.ma2012@gmail.com>
2024-05-23 00:22:08 +0600

    book details styled

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,4 +1,5 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { NavLink, useLoaderData, useParams } from "react-router-dom";
+import Tags from "../Tags/Tags";
 
 
 
@@ -8,11 +9,45 @@ const BookDetails = () => {
     const idInt = parseInt(id);
     const book = books.find(book => book.bookId === idInt);
     console.log(book, id)
+    const {review, image, bookName, author, category, tags, totalPages, publisher, yearOfPublishing, rating} = book;
 
     return (
-        <div>
-            <h2>{book.bookName}</h2>
+        <div className="md:flex md:justify-center md:items-center gap-6 mt-16">
+            <div className="p-16 bg-[#F3F3F3] rounded-2xl w-full md:w-1/2 flex justify-center items-center">
+                <img className="md:w-3/5" src={image} alt="BookCover" />
+            </div>
+            <div className="w-full md:w-1/2">
+            <h2 className="text-4xl font-bold">{bookName}</h2>
+            <p className="mb-3 mt-2">By: {author}</p>
+            <p className="py-2 border-t border-b border-solid">{category}</p>
+            <p className="mt-3"><span className="font-bold">Review: </span> <span className="text-[#131313B3]">{review}</span></p>
+            <div  className="flex
+        space-x-2 items-center border-solid border-b pb-1 mt-4">
+            <p className="font-bold mr-2 pb-5 ">Tag</p>
+                {
+                    tags.map((tag, idx) => <Tags key={idx} tag={tag}></Tags>)
+                }
+            </div>
+            <p className="flex justify-between mt-4">
+                <span className="flex justify-start"> Number of Pages:</span>
+                <span className="font-bold flex justify-end">{totalPages}</span> </p>
 
+                <p className="flex justify-between">
+                <span className="flex justify-start">Publisher: </span>
+                <span className="font-bold flex justify-end">{publisher}</span> </p>
+
+                <p className="flex justify-between">
+                <span className="flex justify-start">Year of Publishing: </span>
+                <span className="font-bold flex justify-end">{yearOfPublishing}</span> </p>
+
+                <p className="flex justify-between">
+                <span className="flex justify-start">Rating: </span>
+                <span className="font-bold flex justify-end">{rating}</span> </p>
+                <div className="flex mt-4 ">
+                    <NavLink><button className="btn btn-outline mr-4">Read</button></NavLink>
+                    <NavLink><button className="btn btn-outline">Wishlist</button></NavLink>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -3,7 +3,7 @@
 const Tags = ({tag}) => {
     return (
         <span className="text-[#23BE0A] bg-[#23BE0A0D] px-3 py-1 mr-2 rounded-md mb-6">
-            {tag}
+            # {tag}
         </span>
     );
 };
